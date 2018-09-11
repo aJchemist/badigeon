f37250175af25ef96ac9b9a1c1a1d961b0ce55a1
aJchemist <valchemist@me.com>
2018-09-11 21:19:16 +0900

    Revised badigeon.install

--- src/badigeon/install.clj
+++ src/badigeon/install.clj
@@ -1,7 +1,12 @@
 (ns badigeon.install
-  (:require [clojure.tools.deps.alpha.util.maven :as maven]
-            [clojure.java.io :as io])
-  (:import [org.eclipse.aether.installation InstallRequest]))
+  (:require
+   [clojure.tools.deps.alpha.util.maven :as maven]
+   [clojure.java.io :as jio]
+   )
+  (:import
+   org.eclipse.aether.installation.InstallRequest
+   ))
+
 
 (defn install
   "Install a jar file into the local maven repository.
@@ -13,18 +18,19 @@
   ([lib maven-coords file-path pom-file-path]
    (install lib maven-coords file-path pom-file-path nil))
   ([lib maven-coords file-path pom-file-path {:keys [local-repo]}]
-   (let [local-repo (or local-repo maven/default-local-repo)
-         system (maven/make-system)
-         session (maven/make-session system local-repo)
-         artifact (maven/coord->artifact lib maven-coords)
-         artifact (.setFile artifact (io/file file-path))
+   (let [local-repo   (or local-repo maven/default-local-repo)
+         system       (maven/make-system)
+         session      (maven/make-session system local-repo)
+         artifact     (maven/coord->artifact lib maven-coords)
+         artifact     (.setFile artifact (jio/file file-path))
          pom-artifact (maven/coord->artifact lib (-> maven-coords
-                                                     (assoc :extension "pom")
-                                                     (dissoc :classifier)))
-         pom-artifact (.setFile pom-artifact (io/file pom-file-path))]
+                                                   (assoc :extension "pom")
+                                                   (dissoc :classifier)))
+         pom-artifact (.setFile pom-artifact (jio/file pom-file-path))]
      (.install system session (-> (InstallRequest.)
                                   (.addArtifact artifact)
                                   (.addArtifact pom-artifact))))))
+
 
 (comment
   (install 'badigeon/badigeon
@@ -33,5 +39,3 @@
            "target/badigeong-0.0.1-SNAPSHOT-cl.jar"
            "pom.xml")
   )
-
-
